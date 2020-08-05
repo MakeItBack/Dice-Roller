dc823d25dc10563daeee5dbd49ac96d0a75167f2
Mike Back <mikeback75@hotmail.com>
2020-08-05 14:33:24 +1000

    Add dice to page layout

---
+++ dice.js
@@ -0,0 +1,8 @@
+// display the correct number of dice - adjust number of cards
+
+// listen for a click of the button
+
+// on click generate random number from 1-6
+
+// display the correct dice images for the numbers generated
+
