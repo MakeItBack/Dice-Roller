b5133f0bff0b097d373e864facac75b4fbef9dc2
Mike Back <mikeback75@hotmail.com>
2020-09-26 14:08:57 +1000

    add d10 dice roll

--- dice.js
+++ dice.js
@@ -25,50 +25,71 @@ $("#inlineRadio3").change(()=>{
 	$(".card-deck").html(addDice1+addDice2+addDice3);
 }});
 
-// listen for a click of the button
-// Loop through the dice images and for each one:
-// generate a random number from 1-6
-// display the correct dice images for the number generated
-
-// $(".btn").click(()=>{
-// 	imgIds.forEach((image)=>{
-// 		let roll = Math.floor(Math.random()*6)+1;
-// 		console.log(image +" rolled "+ roll);
-// 		if (roll === 6) {$(image).attr('src', 'img/six2.png')}
-// 		else if (roll === 5) {$(image).attr('src', 'img/five2.png')}
-// 		else if (roll === 4) {$(image).attr('src', 'img/four2.png')}
-// 		else if (roll === 3) {$(image).attr('src', 'img/three2.png')}
-// 		else if (roll === 2) {$(image).attr('src', 'img/two2.png')}
-// 		else {$(image).attr('src', 'img/one2.png')};
-// 	})
-// });
-
-// Alternative syntax - the SWITCH STATEMENT
-
 $(".btn").click(()=>{
 	imgIds.forEach((image)=>{
-		let roll = Math.floor(Math.random()*6)+1;
-		console.log(image +" rolled "+ roll);
-		$(image).addClass('diceRoll');
-		switch(roll){
-			case 6:
-				$(image).attr('src', 'img/six2.png');
-				break;
-			case 5:
-				$(image).attr('src', 'img/five2.png');
-				break;
-			case 4:
-				$(image).attr('src', 'img/four2.png');
-				break;
-			case 3:
-				$(image).attr('src', 'img/three2.png');
-				break;
-			case 2:
-				$(image).attr('src', 'img/two2.png');
-				break;
-			case 1:
-				$(image).attr('src', 'img/one2.png');
-				break;
+		// Check if d10 switch is off (when switch is off the .val() returns undefined i.e. falsy so reverse to true with ! in front)
+		if (!$("#customSwitch1:checked").val()){
+
+			let roll = Math.floor(Math.random()*6)+1;
+			console.log(image +" rolled "+ roll);
+			$(image).addClass('diceRoll');
+			switch(roll){
+				case 6:
+					$(image).attr('src', 'img/six2.png');
+					break;
+				case 5:
+					$(image).attr('src', 'img/five2.png');
+					break;
+				case 4:
+					$(image).attr('src', 'img/four2.png');
+					break;
+				case 3:
+					$(image).attr('src', 'img/three2.png');
+					break;
+				case 2:
+					$(image).attr('src', 'img/two2.png');
+					break;
+				case 1:
+					$(image).attr('src', 'img/one2.png');
+					break;
+			};
+		}
+		else{
+			let roll = Math.floor(Math.random()*10);
+			console.log(`${image} rolled ${roll} (d10 dice)`);
+			$(image).addClass('diceRoll');
+			switch(roll){
+				case 9:
+					$(image).attr('src', 'img/d10/d10_Final/d10-9.png');
+					break;
+				case 8:
+					$(image).attr('src', 'img/d10/d10_Final/d10-8.png');
+					break;
+				case 7:
+					$(image).attr('src', 'img/d10/d10_Final/d10-7.png');
+					break;
+				case 6:
+					$(image).attr('src', 'img/d10/d10_Final/d10-6.png');
+					break;
+				case 5:
+					$(image).attr('src', 'img/d10/d10_Final/d10-5.png');
+					break;
+				case 4:
+					$(image).attr('src', 'img/d10/d10_Final/d10-4.png');
+					break;
+				case 3:
+					$(image).attr('src', 'img/d10/d10_Final/d10-3.png');
+					break;
+				case 2:
+					$(image).attr('src', 'img/d10/d10_Final/d10-2.png');
+					break;
+				case 1:
+					$(image).attr('src', 'img/d10/d10_Final/d10-1.png');
+					break;
+				case 0:
+					$(image).attr('src', 'img/d10/d10_Final/d10-0.png');
+					break;
+			};
 		};	
 	})
 	// set timer to allow animation to run before removing animation class
@@ -77,19 +98,8 @@ $(".btn").click(()=>{
 
 
 // UPGRADES - ideas
-	// animation for roll - done
 	// Can't press roll button without selecting number of dice first
 	// Background image toggler
-	// Option to change to 10 sided dice
 	// Re-factor!!
 	// work on scaling the background image better at different screen sizes - see Mac / retina
-	// Mobile optimisation - done
-	// Add css prefixes
-
-// What do i need to change to get 10 sided dice
-	// Add a toggle / selector on the HTML file - done
-	// Need to capture selector setting and update a variable with 6 or 10
-	// Update logic with 2 paths based on the variable:
-		// need to roll 0-9
-		// New images for 0-9 - done
 
